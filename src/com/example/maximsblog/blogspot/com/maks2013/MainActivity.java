4fd0dceb49b361617cebc8b186111a2784fdab64
MAKSIM BARTOSH <MKSMBRTSH@GMAIL.COM>
2013-08-20 12:58:55 +0400

    create project

---
+++ src/com/example/maximsblog/blogspot/com/maks2013/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.maximsblog.blogspot.com.maks2013;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
