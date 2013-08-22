2ce26b7233741f7e3ce3a1c3aeb6fa71288decfc
MAKSIM BARTOSH <MKSMBRTSH@GMAIL.COM>
2013-08-22 14:12:50 +0400

    scroll rect scheme

--- src/com/maximsblog/blogspot/com/maks2013/SchemFragment.java
+++ src/com/maximsblog/blogspot/com/maks2013/SchemFragment.java
@@ -20,66 +20,82 @@ import android.widget.TextView;
 
 public class SchemFragment extends Fragment {
 
-	 private float mX;
-	 private float mY;
-	 
-	    public static SchemFragment newInstance(String content) {
-	    	SchemFragment fragment = new SchemFragment();
+	private float mX;
+	private float mY;
 
-	        return fragment;
-	    }
-		
-	    @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
+	public static SchemFragment newInstance(String content) {
+		SchemFragment fragment = new SchemFragment();
 
-	    }
+		return fragment;
+	}
 
-	    @Override
-	    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	       LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.schem_fragment, container, false);
-	       final ImageView schema = (ImageView) layout.findViewById(R.id.schema);
-	       Bitmap bitmap = ((BitmapDrawable)schema.getDrawable()).getBitmap();
-	       
-	       int height = bitmap.getHeight();
-	       int width = bitmap.getWidth();       
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-	       schema.setOnTouchListener(new View.OnTouchListener() {
+	}
 
-	           public boolean onTouch(View arg0, MotionEvent event) {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		LinearLayout layout = (LinearLayout) inflater.inflate(
+				R.layout.schem_fragment, container, false);
+		final ImageView schema = (ImageView) layout.findViewById(R.id.schema);
+		Bitmap bitmap = ((BitmapDrawable) schema.getDrawable()).getBitmap();
 
-	               float curX, curY;
+		final int height = bitmap.getHeight();
+		final int width = bitmap.getWidth();
 
-	               switch (event.getAction()) {
+		schema.setOnTouchListener(new View.OnTouchListener() {
 
-	                   case MotionEvent.ACTION_DOWN:
-	                       mX = event.getX();
-	                       mY = event.getY();
-	                       break;
-	                   case MotionEvent.ACTION_MOVE:
-	                       curX = event.getX();
-	                       curY = event.getY();
-	                       //int sx = schema.getScrollX();
-	                       //int sy = schema.getScrollY();
-	                       schema.scrollBy((int) (mX - curX), (int) (mY - curY));
-	                       mX = curX;
-	                       mY = curY;
-	                       break;
-	                   case MotionEvent.ACTION_UP:
-	                       curX = event.getX();
-	                       curY = event.getY();
-	                       schema.scrollBy((int) (mX - curX), (int) (mY - curY));
-	                       break;
-	               }
+			public boolean onTouch(View arg0, MotionEvent event) {
 
-	               return true;
-	           }
-	       });
-	        return layout;
-	    }
+				float curX, curY;
 
-	    @Override
-	    public void onSaveInstanceState(Bundle outState) {
-	        super.onSaveInstanceState(outState);
-	    }
+				switch (event.getAction()) {
+
+				case MotionEvent.ACTION_DOWN:
+					mX = event.getX();
+					mY = event.getY();
+					break;
+				case MotionEvent.ACTION_MOVE:
+					curX = event.getX();
+					curY = event.getY();
+					int dx = (int) (mX - curX);
+					int dy = (int) (mY - curY);
+					int nx = dx + schema.getScrollX();
+					int ny = dy + schema.getScrollY();
+
+					if (nx + schema.getWidth() > width)
+						dx = 0;
+					if (ny + schema.getHeight()> height)
+						dy = 0;
+					if (nx < 0)
+						dx = 0;
+					if (ny < 0)
+						dy = 0;
+					schema.scrollBy(dx, dy);
+					mX = curX;
+					mY = curY;
+
+					break;
+				case MotionEvent.ACTION_UP:
+					curX = event.getX();
+					curY = event.getY();
+
+					schema.scrollBy((int) (mX - curX), (int) (mY - curY));
+
+					break;
+				}
+
+				return true;
+			}
+		});
+		return layout;
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+	}
 }
