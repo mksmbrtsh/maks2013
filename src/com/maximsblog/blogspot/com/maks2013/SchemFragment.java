1169428140793ca332bb5cd2b30826679ff1327d
MAKSIM BARTOSH <MKSMBRTSH@GMAIL.COM>
2013-08-23 10:27:14 +0400

    add tiles of scheme, fix out of memory, add library Android-Tiling-ScrollView

--- src/com/maximsblog/blogspot/com/maks2013/SchemFragment.java
+++ src/com/maximsblog/blogspot/com/maks2013/SchemFragment.java
@@ -1,16 +1,19 @@
 package com.maximsblog.blogspot.com.maks2013;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
@@ -40,57 +43,14 @@ public class SchemFragment extends Fragment {
 			Bundle savedInstanceState) {
 		LinearLayout layout = (LinearLayout) inflater.inflate(
 				R.layout.schem_fragment, container, false);
-		final ImageView schema = (ImageView) layout.findViewById(R.id.schema);
-		Bitmap bitmap = ((BitmapDrawable) schema.getDrawable()).getBitmap();
-
-		final int height = bitmap.getHeight();
-		final int width = bitmap.getWidth();
-
-		schema.setOnTouchListener(new View.OnTouchListener() {
-
-			public boolean onTouch(View arg0, MotionEvent event) {
-
-				float curX, curY;
-
-				switch (event.getAction()) {
-
-				case MotionEvent.ACTION_DOWN:
-					mX = event.getX();
-					mY = event.getY();
-					break;
-				case MotionEvent.ACTION_MOVE:
-					curX = event.getX();
-					curY = event.getY();
-					int dx = (int) (mX - curX);
-					int dy = (int) (mY - curY);
-					int nx = dx + schema.getScrollX();
-					int ny = dy + schema.getScrollY();
-
-					if (nx + schema.getWidth() > width)
-						dx = 0;
-					if (ny + schema.getHeight()> height)
-						dy = 0;
-					if (nx < 0)
-						dx = 0;
-					if (ny < 0)
-						dy = 0;
-					schema.scrollBy(dx, dy);
-					mX = curX;
-					mY = curY;
-
-					break;
-				case MotionEvent.ACTION_UP:
-					curX = event.getX();
-					curY = event.getY();
-
-					schema.scrollBy((int) (mX - curX), (int) (mY - curY));
-
-					break;
-				}
-
-				return true;
-			}
-		});
+		/*Display display = ((WindowManager)
+                getActivity().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int displayWidth = display.getWidth();             
+        int displayHeight = display.getHeight();    
+        
+		Scheme s = new Scheme(this.getActivity(), displayWidth, displayHeight);
+		//layout.addView(s);
+		*/
 		return layout;
 	}
 
